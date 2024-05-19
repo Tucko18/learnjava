86f39fc854bc481e3bfd868e0b4e29bac5a5fe51
TUCKO <126333648+Tucko18@users.noreply.github.com>
2024-05-19 16:35:17 -0300

    Add files via upload

---
+++ TimeOfTheSystem.java
@@ -0,0 +1,13 @@
+package com.mycompany.timeofthesystem;
+
+import java.util.Date;
+
+public class TimeOfTheSystem {
+
+    public static void main(String[] args) {
+        
+        Date clock = new Date();
+        System.out.println("The hour of the system is ");
+        System.out.println(clock.toString());
+}
+}
